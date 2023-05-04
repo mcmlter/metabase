f2d8a5f454b42d1ad722b0ea0b6ebc47b8e7e467
Renée F. Brown <rfbrown@unm.edu>
2023-05-04 12:46:56 -0600

    build: update locale

--- sql/00_create_db.sql
+++ sql/00_create_db.sql
@@ -6,7 +6,7 @@ CREATE DATABASE metabase
     WITH 
     OWNER = mcmlter -- You must substitute the username before running this.
     ENCODING = 'UTF8'
-    LC_COLLATE = 'en_US.UTF-8'
-    LC_CTYPE = 'en_US.UTF-8'
+    LC_COLLATE = 'C.UTF-8'
+    LC_CTYPE = 'C.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
